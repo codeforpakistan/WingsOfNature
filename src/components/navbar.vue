12865bc239f35d54b36a1c6be1298b21a9b49802
aliirz <dejavu87@gmail.com>
2023-10-26 23:08:10 +0500

    navbar logo click to main page

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="" class="flex items-center">
+      <a href="/" class="flex items-center">
           <span class="text-4xl font-bold text-blue-500 mr-2">W</span>
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Wings of Nature</span>
       </a>
