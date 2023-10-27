3c180fd71c1b5c4016b8ad83939829c7175856ad
aliirz <dejavu87@gmail.com>
2023-10-27 13:16:57 +0500

    fixed a few issues with dark mode

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="border-gray-200 bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" class="flex items-center">
           <span class="text-4xl font-bold text-blue-500 mr-2">W</span>
@@ -12,7 +12,7 @@
           </svg>
       </button>
       <div :class="{'hidden': !menuOpen, 'block': menuOpen, 'w-full': true, 'md:block': true, 'md:w-auto': true}" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="bg-gray-900 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-dark-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
             <router-link to="/" class="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="{'bg-blue-700 text-white': $route.path === '/'}">Home</router-link>
 
